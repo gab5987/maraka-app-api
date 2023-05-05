0c2357e03c86f6495d4f60ec114d98b730ff5e18
gab5987 <biell.gb4@gmail.com>
2023-05-04 23:23:00 -0300

    Fix: Cors Policy

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
@@ -8,6 +9,7 @@ import (
 	"maraka/booking"
 	"maraka/db"
 	"os"
+	"time"
 )
 
 func main() {
@@ -17,6 +19,27 @@ func main() {
 		}
 	}
 	router := gin.Default()
+
+	headers := []string{
+		"Content-Type",
+		"Content-Length",
+		"Accept-Encoding",
+		"X-CSRF-Token",
+		"Authorization",
+		"accept",
+		"origin",
+		"Cache-Control",
+		"X-Requested-With",
+	}
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+		AllowHeaders:     headers,
+		ExposeHeaders:    headers,
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	db.Init(os.Getenv("DB_URL"))
 
